071a5f336b76c24ee72b4aff7537604777b62fd2
John Esrom <johnmatos@3coracoes.com.br>
2025-08-18 17:55:36 -0300

    fix: deletado comentario

--- app/auth-provider.tsx
+++ app/auth-provider.tsx
@@ -20,7 +20,6 @@ export default function AuthProvider({children} : {children: React.ReactNode}) {
 
     console.log("AuthProvider currentUser:", currentUser);
 
-    // Render nothing while checking auth state
     if (currentUser === undefined) {
         return null;
     }
