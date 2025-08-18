4ef6120c2c3ff3b852f7939c6408d9b2ca909fe3
John Esrom <johnmatos@3coracoes.com.br>
2025-08-18 20:31:20 -0300

    feat: adicionado nome do usuario logado no dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,18 @@
 "use client"
 
+import { useSelector } from "react-redux";
 import { Topbar } from "../../components/TopBar"
+import { RootState } from "@/redux/store";
 
 export default function DashboardPage() {
+
+  const { currentUser } = useSelector((state: RootState) => state.userReducer);
+
   return (
     <div className="min-h-screen bg-gray-100">
       <Topbar />
       <main className="max-w-6xl mx-auto px-4 py-6">
-        <h2 className="text-2xl font-bold mb-4">Bem-vindo ao Dashboard!</h2>
+        <h2 className="text-2xl font-bold mb-4">Bem-vindo ao Dashboard! {currentUser}</h2>
         <p className="text-gray-700">
           Esta é uma tela simples de dashboard.  
           Aqui você pode adicionar cards, gráficos ou tabelas futuramente.
