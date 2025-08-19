ef319540ac6588842c515cf2d65185b42be4d4fd
John Esrom <johnmatos@3coracoes.com.br>
2025-08-19 09:08:59 -0300

    fix: dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -18,6 +18,9 @@ export default function DashboardPage() {
           Esta é uma tela simples de dashboard.  
           Aqui você pode adicionar cards, gráficos ou tabelas futuramente.
         </p>
+
+
+        
       </main>
     </div>
   )
